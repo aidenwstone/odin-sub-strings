ccedf97544b874bf5a94d894378473f7dd34def8
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-06-23 21:56:03 -0400

    Refactor substrings method for readability

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,9 +1,10 @@
 def substrings(phrase, substrings)
   # Take a phrase and array of substrings as arguments
   # Return a hash of each found substring & how many times it was found
-  substrings.reduce(Hash.new) do |found_strings, string|
-    string_count = phrase.downcase.scan(string).length
+  downcased_phrase = phrase.downcase
+
+  substrings.each_with_object(Hash.new) do |string, found_strings|
+    string_count = downcased_phrase.scan(string).length
     found_strings[string] = string_count if string_count > 0
-    found_strings
   end
 end
