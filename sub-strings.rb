bc9047b565559d454f4c8793ca2d5f1bf89c5030
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-06-23 21:44:16 -0400

    Implement substrings method

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,4 +1,9 @@
 def substrings(phrase, substrings)
   # Take a phrase and array of substrings as arguments
   # Return a hash of each found substring & how many times it was found
+  substrings.reduce(Hash.new) do |found_strings, string|
+    string_count = phrase.downcase.scan(string).length
+    found_strings[string] = string_count if string_count > 0
+    found_strings
+  end
 end
